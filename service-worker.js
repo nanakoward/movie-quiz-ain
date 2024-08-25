f9b141514bf33efd5a8aa45753b224cd298c0b76
nanakoward <drawok88@gmail.com>
2024-08-25 17:08:09 +0900

    asdf

--- service-worker.js
+++ service-worker.js
@@ -17,11 +17,24 @@ self.addEventListener('install', event => {
   );
 });
 
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(response => {
+//         return response || fetch(event.request);
+//       })
+//   );
+// });
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      caches.match(event.request)
+          .then(response => {
+              return response || fetch(event.request).then(response => {
+                  return caches.open(CACHE_NAME).then(cache => {
+                      cache.put(event.request, response.clone());
+                      return response;
+                  });
+              });
+          })
   );
 });
